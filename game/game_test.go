7dfbda73c69256ca88f8819029474629a68366ad
Jack Oddy <joddy@andigital.com>
2017-10-03 12:37:28 +0100

    :squirrel: adds table driven test for game creation generated content

---
+++ game/game_test.go
@@ -0,0 +1,32 @@
+package game
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestCreateGameGeneration(t *testing.T) {
+	testGeneratedStuff := map[string]string{"Word": "TestWord", "Id": "TestId"}
+
+	for key, expected := range testGeneratedStuff {
+		SillyStuff[key] = func() string { return expected }
+	}
+
+	game := CreateGame()
+
+	for key, word := range testGeneratedStuff {
+		value := getField(game, key)
+
+		if value != word {
+			t.Errorf("expected a newly created game %s to have a value of %s. Instead got %s",
+				key, word, value)
+		}
+	}
+
+}
+
+func getField(g Game, field string) string {
+	r := reflect.ValueOf(g)
+	f := reflect.Indirect(r).FieldByName(field)
+	return string(f.String())
+}
